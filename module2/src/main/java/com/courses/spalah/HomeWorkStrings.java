890ecbb55af32cb7457a853d19a877a8c5bf183e
Stanislav Pugach <staspugachover900@gmail.com>
2016-07-06 18:44:49 +0300

    Home Work 1
    Done with loops and string home work

--- module2/src/main/java/com/courses/spalah/HomeWorkStrings.java
+++ module2/src/main/java/com/courses/spalah/HomeWorkStrings.java
@@ -15,7 +15,13 @@ public class HomeWorkStrings {
      * @return количество четных числе в строке
      */
     public static int countEvenInString(String s) {
-        return 1;
+
+        String[] ArrayOfNumbers = s.split("_");
+        int count = 0;
+        for (String num : ArrayOfNumbers) {
+            if (Integer.parseInt(num) % 2 == 0) count++;
+        }
+        return count;
     }
 
     /**
@@ -29,6 +35,38 @@ public class HomeWorkStrings {
      * @return результирующая строка
      */
     public static String removeSymbolFromString(String symbol, String s) {
-        return "";
+
+        // check if the string contain center position and assign the number of index
+        boolean centerPositionExist = false;
+        int indexOfCenterPosition = 0;
+        if (s.length() % 2 != 0){
+            centerPositionExist = true;
+            indexOfCenterPosition = (s.length() - 1) / 2;
+        }
+
+        String[] arrayOfStrings = s.split("");
+        StringBuilder newString = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+
+            //adding strings in the firs and the last position in any case
+            if (i == 0 || i == s.length() - 1) {
+                newString.append(arrayOfStrings[i]);
+                continue;
+            }
+            //if we have center position and variable i in the loop in that position, we appending it
+            if (centerPositionExist && i == indexOfCenterPosition){
+                newString.append(arrayOfStrings[i]);
+            }
+            //if not, we check
+            else if (!symbol.equals(arrayOfStrings[i])) {
+                newString.append(arrayOfStrings[i]);
+            }
+
+        }
+        return newString.toString();
+        /*
+        кажется, что сильно грамоздко получилось
+         */
     }
 }
