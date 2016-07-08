66edc686f5787c23d183e2ef5c2146d2cdd17e8c
Stanislav Pugach <staspugachover900@gmail.com>
2016-07-08 16:58:20 +0300

    Home Work 2
    Corrected method removeSymbolFromString

--- module2/src/main/java/com/courses/spalah/HomeWorkStrings.java
+++ module2/src/main/java/com/courses/spalah/HomeWorkStrings.java
@@ -36,37 +36,26 @@ public class HomeWorkStrings {
      */
     public static String removeSymbolFromString(String symbol, String s) {
 
-        // check if the string contain center position and assign the number of index
-        boolean centerPositionExist = false;
-        int indexOfCenterPosition = 0;
-        if (s.length() % 2 != 0){
-            centerPositionExist = true;
-            indexOfCenterPosition = (s.length() - 1) / 2;
-        }
-
         String[] arrayOfStrings = s.split("");
         StringBuilder newString = new StringBuilder();
 
         for (int i = 0; i < s.length(); i++) {
 
-            //adding strings in the firs and the last position in any case
-            if (i == 0 || i == s.length() - 1) {
+            //adding strings in the firs and the last position in any case and middle if we have
+            if (i == 0 || i == s.length() - 1 || i == indexOfCenterPosition(s)) {
                 newString.append(arrayOfStrings[i]);
                 continue;
-            }
-            //if we have center position and variable i in the loop in that position, we appending it
-            if (centerPositionExist && i == indexOfCenterPosition){
-                newString.append(arrayOfStrings[i]);
-            }
-            //if not, we check
-            else if (!symbol.equals(arrayOfStrings[i])) {
+            } else if (!symbol.equals(arrayOfStrings[i])) {
                 newString.append(arrayOfStrings[i]);
             }
 
         }
         return newString.toString();
-        /*
-        кажется, что сильно грамоздко получилось
-         */
+    }
+
+    private static int indexOfCenterPosition(String s) {
+
+        if (s.length() % 2 != 0) return (s.length() - 1) / 2;
+        else return 0;
     }
 }
